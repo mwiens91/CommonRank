984cc2328dfd8efffd5e941579a12bc181ccbe14
Avery Jones <averyj@sfu.ca>
2018-02-17 14:39:03 -0800

    Adds leaderboard functionality as well as additional member variables

--- core/models.py
+++ core/models.py
@@ -23,8 +23,50 @@ class Leaderboard(models.Model):
     name = models.CharField(max_length=200,
                             null=True,
                             blank=False,)
+    challenge_enabled = models.BooleanField(null=True, blank=True)
+
+    deadline_time = models.IntegerField(blank=2100, null=True)
+    deadline_length = models.IntegerField(blank=14, null=True)
+    elo_sensitivity = models.FloatField(blank=0.5, null=False)
+
+
     def __str__(self):
         return '%s' % self.name
+
+    def invite_member(self, profile):
+        Member.objects.create(leaderboard=self, profileuser=profile)
+
+    def delete_member(self, profile):
+        Member.objects.get(profileuser=profile).delete()
+
+    def enable_challenge(self):
+        self.challenge_enabled = True
+
+    def disable_challenge(self):
+        self.challenge_enabled = False
+
+    def toggle_challenge(self):
+        if self.challenge_enabled == True:
+            self.disable_challenge()
+        else:
+            self.enable_challenge()
+
+    def set_challenge_deadline(self, hour, minute):
+        self.deadline_time = (hour * 100) + minute
+
+    def set_challenge_duration(self, days):
+        self.deadline_length = days
+
+    def set_sensitivity(self, sensitivity):
+        self.elo_sensitivity = sensitivity
+
+    def add_privilege(self, user):
+        subject = Member.objects.get(profileuser=user)
+        subject.increase_privilege()
+
+    def remove_privilege(self, user):
+        subject = Member.objects.get(profileuser=user)
+        subject.decrese_privilege()
 
 class Member(models.Model):
 
@@ -37,6 +79,7 @@ class Member(models.Model):
                                     null=True,
                                     blank=True,)
     elo = models.FloatField()
+    privilege = models.IntegerField(default=1, null=False)
 
 class Challenge(models.Model):
 
