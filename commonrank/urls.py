9a6d9ffbad97ecc7da878336418402c1d6bf0c77
Avery Jones <averyj@sfu.ca>
2018-02-18 03:10:39 -0800

    updated urls to properly include invitations

--- commonrank/urls.py
+++ commonrank/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 from core import views as core_views
 
 urlpatterns = [
