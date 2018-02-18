bc30c7200118342428fb83e3494f954b17e75111
Branko Bajcetic <bajcetic.branko@gmail.com>
2018-02-18 08:25:09 -0800

    Update number of probation games easily

--- core/update_elo.py
+++ core/update_elo.py
@@ -1,8 +1,10 @@
 import math
 #from core.models import Leaderboard, Member, Profile, User, Match
 
+
 #winner and loser are members, elo_sensitivity is a float from 0 to 1 inclusive
 def update_elo(winner, loser, elo_sensitivity):
+    GAMES_IN_PROBATION = 20
     winner_games = winner.wins + winner.losses
     loser_games = loser.wins + loser.losses
     if elo_sensitivity < 0.167:
@@ -21,17 +23,17 @@ def update_elo(winner, loser, elo_sensitivity):
     loser_k_factor = k_factor
     expected_winner = 1/(1+math.pow(10,((loser.elo-winner.elo)/400)))
     expected_loser = 1 - expected_winner
-    if(winner_games<20 and winner_games>=10):
+    if(winner_games<GAMES_IN_PROBATION and winner_games>=GAMES_IN_PROBATION//2):
         winner_k_factor = winner_k_factor*2
-    elif(winner_games<10 and winner_games>=0):
+    elif(winner_games<GAMES_IN_PROBATION//2 and winner_games>=0):
         winner_k_factor = winner_k_factor*4
-    if(loser_games<20 and loser_games>=10):
+    if(loser_games<GAMES_IN_PROBATION and loser_games>=GAMES_IN_PROBATION//2):
         loser_k_factor = loser_k_factor*2
-    elif(loser_games<10 and loser_games>=0):
+    elif(loser_games<GAMES_IN_PROBATION//2 and loser_games>=0):
         loser_k_factor = loser_k_factor*4
 
-    loser.elo = loser.elo + loser_k_factor*(0-expected_loser)
-    winner.elo = winner.elo + winner_k_factor*(1-expected_winner)
+    loser.elo += loser_k_factor*(0-expected_loser)
+    winner.elo += winner_k_factor*(1-expected_winner)
 
     # Increment wins and losses
     loser.losses += 1
