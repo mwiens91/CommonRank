b2e535587074913852ab611bc4dcf38d35211c9c
Avery Jones <averyj@sfu.ca>
2018-02-18 03:14:17 -0800

    initialized invitations

--- commonrank/settings.py
+++ commonrank/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'timezone_field',
     'core',
+    'invitations',
 ]
 
 MIDDLEWARE = [
