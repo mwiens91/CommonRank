6109930dd631bb6135afe79be26cdd3d421837a4
Matt Wiens <mwiens91@gmail.com>
2018-02-17 19:31:09 -0800

    Fix typo in leaderboard home view

--- core/views.py
+++ core/views.py
@@ -38,7 +38,7 @@ def leaderboard_home(request, leaderboard_id):
     return render(request,
                   'leaderboard_home.html',
                   {'leaderboard': thisleaderboard,
-                   'topmemebers': toplist})
+                   'topmembers': toplist})
 
 @login_required
 def profile_home(request):
